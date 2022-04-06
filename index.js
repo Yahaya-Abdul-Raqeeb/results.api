eeee1228fa0a1daceaa4d3a3b2c68acde866caa8
unknown <yahayaabdulraqeeb363@gmail.com>
2022-04-06 14:40:12 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const dotenv =require("dotenv")
+const results=require("./results");
+const res = require("express/lib/response");
+const req = require("express/lib/request");
+
+
+
+dotenv.config()
+
+
+const app=express();
+
+//middlewares
+app.use(express.json())
+
+
+//route
+//get all results
+app.get("/results", (req, res)=>{
+    res.json(results)
+})
+
+
+//post a results
+app.post("/results", (req, res)=>{
+const students ={
+name:req.body.name,
+class:req.body.class,
+marks:req.body.marks
+};
+
+results.push(students);
+res.json(results)
+})
+
+//delete a results
+app.delete("/results/:name", (req, res)=>{
+    res.json(results.filter((result)=>result.name!==req.params.name))
+})
+
+
+const PORT =process.env.PORT||5000
+app.listen(PORT, ()=>console.log(`server is running on port ${PORT}`))
---
+++ results.js
@@ -0,0 +1,12 @@
+const results=[
+{Name:"Dasaah McFlex", Class:"form one", Marks:-10},
+{Name:"Abdul Raqeeb Icon", Class:"form one", Marks:9},
+{Name:"Saeed Baaqi", Class:"form one", Marks:8},
+{Name:"Ali Subur", Class:"form one", Marks:6},
+{Name:"Ali Anis", Class:"form one", Marks:7},
+{Name:"Majeed Aisha", Class:"form one", Marks:5},
+{Name:"Dumba Theresa", Class:"form one", Marks:3},
+{Name:"Zakaria Mubariq", Class:"form one", Marks:4},
+
+]
+module.exports=results
